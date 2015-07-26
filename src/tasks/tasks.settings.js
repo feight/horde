ae9fbc1a83b679003a0698cdd99752d74c267266
Peter Bodechtel <sweetlikepete@gmail.com>
2015-07-26 11:55:42 +0200

    added settings module to horde

---
+++ src/tasks/tasks.settings.js
@@ -0,0 +1,49 @@
+
+
+/* ------------------------------------------------------------------------ */
+/*
+        Module to collect settings related tasks
+*/
+/* ------------------------------------------------------------------------ */
+
+
+module.exports = function(grunt){
+
+
+    /* -------------------------------------------------------------------- */
+    /*
+            public
+    */
+    /* -------------------------------------------------------------------- */
+
+
+    this.compile = function(paths, formats){
+
+        var extend = require("node.extend");
+        var fs = require("fs");
+
+        var settings = {};
+
+        for(var i = 0; i < paths.length; i++){
+
+            paths[i] = paths[i].format(formats);
+
+            if(fs.existsSync(paths[i])){
+
+                try{
+                    settings = extend(true, settings, JSON.parse(fs.readFileSync(paths[i], "utf8")));
+                }catch(e){
+                    grunt.fail.fatal("Configuration file is not valid JSON: " + paths[i]);
+                }
+
+            }
+
+        }
+
+        return settings;
+
+    };
+
+    return this;
+
+};
